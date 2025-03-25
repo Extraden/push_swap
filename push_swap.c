6c84f651f2610b32d0c6b3a78dfe5d59c79d0926
Denis Semenov <dsemenov@student.42lyon.fr>
2025-03-25 19:20:37 +0100

    some changes

--- push_swap.c
+++ push_swap.c
@@ -3,42 +3,87 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+t_result	data_init(t_data* data, int capacity) {
+	if (stack_init(&data->stack_a, capacity) == FAILURE)
+		return FAILURE;
+	if (stack_init(&data->stack_b, capacity) == FAILURE) {
+		stack_destroy(&data->stack_a);
+		return FAILURE;
+	}
+	return SUCCESS;
+}
+
+t_result	stack_init(t_stack* stack, int capacity) {
+	stack->length = 0;
+	stack->capacity = capacity;
+	stack->nums = malloc(capacity * sizeof(int));
+	if (stack->nums == NULL)
+		return FAILURE;
+	return SUCCESS;
+}
+
 int	main(int argc, char **argv)
 {
 	t_data	data;
 	t_stack	stack;
 	int		i;
 
-	data.stack_a.length = argc - 1;
-	data.stack_a.size = data.stack_a.length * (sizeof(int));
-	data.stack_a.nums = malloc(data.stack_a.size);
+	if (data_init(&data, argc - 1) == FAILURE)
+		return -1;
 	i = 0;
 	while (i < data.stack_a.length)
 	{
 		data.stack_a.nums[i] = ft_atoi(argv[i + 1]);
 		i++;
 	}
+	printf("Stack a before: ");
 	i = 0;
 	while (i < data.stack_a.length)
 	{
 		printf("%d ", data.stack_a.nums[i]);
 		i++;
 	}
-	printf("Size of stack a: %d\n", data.stack_a.length);
+	printf("\n");
+
+	printf("Length of stack a: %d\n", data.stack_a.length);
+	printf("Size of stack a: %d\n\n", data.stack_a.capacity);
+
+	printf("Stack b before: ");
+	i = 0;
+	while (i < data.stack_b.length)
+	{
+		printf("%d ", data.stack_b.nums[i]);
+		i++;
+	}
+	printf("\n");
+
+	printf("Length of stack a: %d\n", data.stack_b.length);
+	printf("Size of stack a: %d\n\n", data.stack_b.capacity);
+
+
+	printf("------------------------------------------------------------\n\n");
+
 
 	i = 0;
-	while (i < data.stack_a.length - 1)
+	while (i < 6)
 	{
-		swap(&data.stack_a);
+		push(&data.stack_a, &data.stack_b);
 		i++;
 	}
 
+	printf("Stack a after: ");
 	i = 0;
 	while (i < data.stack_a.length)
 	{
 		printf("%d ", data.stack_a.nums[i]);
 		i++;
 	}
+	printf("\n");
 
+	printf("Length of stack a: %d\n", data.stack_a.length);
+	printf("Size of stack a: %d\n\n\n", data.stack_a.capacity);
+
+
+	printf("\n\n\n");
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -1,12 +1,19 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+typedef enum	e_result {
+	SUCCESS = 0,
+	FAILURE = 1,
+} 				t_result;
+
 typedef struct	s_stack
 {
 	int	*nums;
 	int	length;
-	int	size;
+	int	capacity;
+	char letter;
 } t_stack;
+
 typedef struct	s_data
 {
 	t_stack	stack_a;
@@ -16,5 +23,6 @@ typedef struct	s_data
 void	swap(t_stack *stack);
 void	rotate(t_stack *stack);
 void	reverse_rotate(t_stack *stack);
+void	push(t_stack *from, t_stack *to);
 
 #endif
