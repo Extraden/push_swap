bdcf7bd598efc748833ead1a9d58b3815d6f083a
Denis Semenov <dsemenov@student.42lyon.fr>
2025-04-01 15:25:37 +0200

    Improve parsing

--- src/parse.c
+++ src/parse.c
@@ -6,12 +6,47 @@
 /*   By: dsemenov <dsemenov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 19:08:53 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/03/31 19:35:02 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/04/01 15:25:18 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "push_swap.h"
+#include <stddef.h>
+#include <limits.h>
+
+static t_result has_no_duplicates(int argc, char **argv)
+{
+  size_t  i;
+  size_t  j;
+  size_t  len;
+  char  *arg1;
+  char  *arg2;
+
+  i = 0;
+  while (i < (size_t) argc - 1)
+  {
+    arg1 = argv[i];
+    len = ft_strlen(arg1) + 1;
+    j = i + 1;
+    while (j < (size_t) argc)
+    {
+      arg2 = argv[j];
+      if (ft_strncmp(arg1, arg2, len) == 0)
+        return (FAILURE);
+      j++;
+    }
+    i++;
+  }
+  return (SUCCESS);
+}
+
+
+//TODO check if int and no overflow
+/*static t_result is_int()*/
+/*{*/
+/**/
+/*}*/
 
 static t_result  is_valid_number(char *argument)
 {
@@ -23,9 +58,7 @@ static t_result  is_valid_number(char *argument)
   while (argument[i] != '\0')
   {
     if (argument[i] < '0' || argument[i] > '9')
-    {
       return (FAILURE);
-    }
     i++;
   }
   return (SUCCESS);
@@ -40,7 +73,7 @@ t_result  parse(int argc, char **argv)
   while (i < argc)
   {
     argument = argv[i];
-    if (is_valid_number(argument) == FAILURE)
+    if (is_valid_number(argument) == FAILURE || has_no_duplicates(argc, argv) == FAILURE)
     {
         ft_putendl_fd("Error", 1);
         return (FAILURE);
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dsemenov <dsemenov@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 18:46:05 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/03/31 19:25:14 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/04/01 11:21:48 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,20 +26,20 @@ static t_result	stack_init(t_stack* stack, int capacity) {
 	stack->length = capacity;
 	stack->nums = malloc(capacity * sizeof(int));
 	if (stack->nums == NULL)
-		return FAILURE;
-	return SUCCESS;
+		return (FAILURE);
+	return (SUCCESS);
 }
 
 static t_result	data_init(t_data* data, int capacity) {
 	if (stack_init(&data->stack_a, capacity) == FAILURE)
-		return FAILURE;
+		return (FAILURE);
 	if (stack_init(&data->stack_b, capacity) == FAILURE)
   {
 		stack_destroy(&data->stack_a);
-		return FAILURE;
+		return (FAILURE);
 	}
   data->stack_b.length = 0;
-	return SUCCESS;
+	return (SUCCESS);
 }
 
 static t_result stack_fill_from_argv(t_data *data, char **argv)
@@ -47,7 +47,7 @@ static t_result stack_fill_from_argv(t_data *data, char **argv)
   int i;
 
 	i = 0;
-	while (i < data->stack_a.length)
+	while (i < data->stack_a.capacity)
 	{
 		data->stack_a.nums[i] = ft_atoi(argv[i + 1]);
 		i++;
@@ -65,7 +65,8 @@ int	main(int argc, char **argv)
     return (1);
   stack_fill_from_argv(&data, argv);
   print_stacks(&data);
-
+  swap(&data.stack_a);
+  push(&data.stack_a, &data.stack_b);
 	printf("\n");
   print_stacks(&data);
 	return (0);
