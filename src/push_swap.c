bfe66cce562d4cae0a4f9e55c76095050f6582e1
Denis Semenov <dsemenov@student.42lyon.fr>
2025-04-04 17:47:36 +0200

    Add malloc failure checks

--- src/push_swap.c
+++ src/push_swap.c
@@ -3,49 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsemenov <dsemenov@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 18:46:05 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/04/03 19:42:03 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/04/04 17:46:45 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
 #include "libft.h"
-#include <stdlib.h>
+#include "push_swap.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-static t_result has_no_duplicates(t_stack *stack)
+static t_result	has_no_duplicates(t_stack *stack)
 {
-  size_t  i;
-  size_t  j;
-  int     first_num;
-  int     second_num;
+	size_t	i;
+	size_t	j;
+	int		first_num;
+	int		second_num;
 
-  i = 0;
-  while (i < stack->length - 1)
-  {
-    first_num = stack->nums[i];
-    j = i + 1;
-    while (j < stack->length)
-    {
-      second_num = stack->nums[j];
-      if (first_num == second_num)
-        return (FAILURE);
-      j++;
-    }
-    i++;
-  }
-  return (SUCCESS);
+	i = 0;
+	while (i < stack->length - 1)
+	{
+		first_num = stack->nums[i];
+		j = i + 1;
+		while (j < stack->length)
+		{
+			second_num = stack->nums[j];
+			if (first_num == second_num)
+				return (FAILURE);
+			j++;
+		}
+		i++;
+	}
+	return (SUCCESS);
 }
 
-static t_result  stack_destroy(t_stack *stack)
+static t_result	stack_destroy(t_stack *stack)
 {
-  free(stack->nums);
-  return (SUCCESS);
+	free(stack->nums);
+	return (SUCCESS);
 }
 
-static t_result	stack_init(t_stack* stack, int capacity) {
+static t_result	stack_init(t_stack *stack, int capacity)
+{
 	stack->capacity = capacity;
 	stack->length = capacity;
 	stack->nums = malloc(capacity * sizeof(int));
@@ -54,30 +55,35 @@ static t_result	stack_init(t_stack* stack, int capacity) {
 	return (SUCCESS);
 }
 
-static t_result	data_init(t_data* data, int capacity) {
+static t_result	data_init(t_data *data, int capacity)
+{
 	if (stack_init(&data->stack_a, capacity) == FAILURE)
 		return (FAILURE);
 	if (stack_init(&data->stack_b, capacity) == FAILURE)
-  {
+	{
 		stack_destroy(&data->stack_a);
 		return (FAILURE);
 	}
-  data->stack_b.length = 0;
+	data->stack_b.length = 0;
 	return (SUCCESS);
 }
 
-static t_result stack_fill_from_argv(t_data *data, char **argv)
+static t_result	stack_fill_from_argv(t_data *data, char **argv)
 {
-  int i;
+	int	i;
 
 	i = 0;
 	while (i < data->stack_a.capacity)
 	{
 		if (ft_atoi_checked(argv[i + 1], &data->stack_a.nums[i]) == FAILURE)
-      return (FAILURE);
+		{
+			stack_destroy(&data->stack_a);
+			stack_destroy(&data->stack_b);
+			return (FAILURE);
+		}
 		i++;
 	}
-  return (SUCCESS);
+	return (SUCCESS);
 }
 
 int	main(int argc, char **argv)
@@ -85,21 +91,23 @@ int	main(int argc, char **argv)
 	t_data	data;
 
 	if (data_init(&data, argc - 1) == FAILURE)
-    return (1);
-  if (stack_fill_from_argv(&data, argv) == FAILURE)
-  {
-    ft_putendl_fd("Error", 1);
-    return (1);
-  }
-  if (has_no_duplicates(&data.stack_a) == FAILURE)
-  {
-    ft_putendl_fd("Error", 1);
-    return (1);
-  }
-  print_stacks(&data);
-  swap(&data.stack_a);
-  push(&data.stack_a, &data.stack_b);
-	printf("\n");
-  print_stacks(&data);
+		return (1);
+	if (stack_fill_from_argv(&data, argv) == FAILURE)
+	{
+		ft_putendl_fd("Error", 1);
+		return (1);
+	}
+	if (has_no_duplicates(&data.stack_a) == FAILURE)
+	{
+		ft_putendl_fd("Error", 1);
+		return (1);
+	}
+	print_stacks(&data);
+	// swap(&data.stack_a);
+	// push(&data.stack_a, &data.stack_b);
+	// printf("\n");
+	// print_stacks(&data);
+	stack_destroy(&data.stack_a);
+	stack_destroy(&data.stack_b);
 	return (0);
 }
--- src/utils.c
+++ src/utils.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsemenov <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 14:10:05 by dsemenov          #+#    #+#             */
-/*   Updated: 2025/04/03 19:28:59 by dsemenov         ###   ########.fr       */
+/*   Updated: 2025/04/04 17:46:39 by dsemenov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <limits.h>
 #include "push_swap.h"
+#include <limits.h>
 
 static const char	*skip_spaces_and_sign(const char *nptr, int *sign)
 {
@@ -47,6 +47,6 @@ t_result	ft_atoi_checked(const char *nptr, int *num)
 	res *= sign;
 	if (res > INT_MAX || res < INT_MIN)
 		return (FAILURE);
-	*num = (int) res;
+	*num = (int)res;
 	return (SUCCESS);
 }
